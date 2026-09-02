afa8c04a84660588ed7537652797e526484a52ce
KiwiMarley <kiwimarley053@gmail.com>
2026-09-02 07:38:45 -0400

    Update Drai.py

--- Drai.py
+++ Drai.py
@@ -26,6 +26,7 @@ blockchain_providers = config["blockchain_providers"]
 # Initialize web3 for Ethereum
 from web3 import Web3, HTTPProvider
 w3 = Web3(AsyncHTTPProvider(blockchain_providers["ETH"]))
+w3 = Web3(HTTPProvider(blockchain_providers["ETH"]))
 w3.eth.default_account = Account.from_key(private_key).address
 
 # Function to get balance of a token on a chain
